8cc005de54f908004eb5c53b9ba039a04e9cab57
Artem's Macbook Pro 14 <macbookpro@MacBook-Pro-MacBook.local>
2024-05-23 13:42:40 +0300

    lecture structs and taits complete

--- exercise/f_structs_traits/src/main.rs
+++ exercise/f_structs_traits/src/main.rs
@@ -1,25 +1,17 @@
+#![warn(clippy::all, clippy::pedantic)]
 // 1. Define a trait named `Bite`
 //
 // Define a single required method, `fn bite(self: &mut Self)`.  We will call this method when we
 // want to bite something.  Once this trait is defined, you should be able to run the program with
 // `cargo run` without any errors.
 //
-//  trait Bite...
-
-
 // 2. Now create a struct named Grapes with a field that tracks how many grapes are left.  If you
 // need a hint, look at how it was done for Carrot at the bottom of this file (you should probably
 // use a different field, though).
 //
-// #[derive(Debug)] // include this line right before your struct definition
-// struct Grapes...
-
-
 // 3. Implement Bite for Grapes.  When you bite a Grapes, subtract 1 from how many grapes are left.
 // If you need a hint, look at how it was done for Carrot at the bottom of this file.
 //
-// impl Bite for...
-
 
 fn main() {
     // Once you finish #1 above, this part should work.
@@ -29,10 +21,10 @@ fn main() {
 
     // 4. Uncomment and adjust the code below to match how you defined your
     // Grapes struct.
-    //
-    //let mut grapes = Grapes { amount_left: 100 };
-    //grapes.bite();
-    //println!("Eat a grape: {:?}", grapes);
+    
+    let mut grapes = Grapes { amount_left: 100 };
+    grapes.bite();
+    println!("Eat a grape: {:?}", grapes);
 
     // Challenge: Uncomment the code below. Create a generic `bunny_nibbles`
     // function that:
@@ -41,8 +33,18 @@ fn main() {
     // Hint: Define the generic type between the function name and open paren:
     //       fn function_name<T: Bite>(...)
     //
-    //bunny_nibbles(&mut carrot);
-    //println!("Bunny nibbles for awhile: {:?}", carrot);
+    bunny_nibbles(&mut carrot);
+    println!("Bunny nibbles for awhile: {:?}", carrot);
+    bunny_nibbles(&mut grapes);
+    println!("Bunny nibbles for awhile: {:?}", grapes);
+    bunny_nibbles(&mut grapes);
+    println!("Bunny nibbles for awhile: {:?}", grapes);
+    bunny_nibbles(&mut grapes);
+    println!("Bunny nibbles for awhile: {:?}", grapes);
+    bunny_nibbles(&mut grapes);
+    println!("Bunny nibbles for awhile: {:?}", grapes);
+    bunny_nibbles(&mut grapes);
+    println!("Bunny nibbles for awhile: {:?}", grapes);
 }
 
 #[derive(Debug)] // This enables using the debugging format string "{:?}"
@@ -50,9 +52,28 @@ struct Carrot {
     percent_left: f32,
 }
 
+#[derive(Debug)] // include this line right before your struct definition
+struct Grapes {
+    amount_left: i32,
+}
+
+trait Bite {
+    fn bite(self: &mut Self) {}
+}
+
+impl Bite for Grapes {
+    fn bite(self: &mut Self) {
+        self.amount_left -= 1;
+    }
+}
+
 impl Bite for Carrot {
     fn bite(self: &mut Self) {
         // Eat 20% of the remaining carrot. It may take awhile to eat it all...
         self.percent_left *= 0.8;
     }
+}
+
+fn bunny_nibbles<T: Bite>(food: &mut T) {
+    food.bite();
 }
